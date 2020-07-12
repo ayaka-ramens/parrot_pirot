9c2fd0844a0864ca83065bd5b2dd16fa27807157
everyleaf <a.moronaga@everyleaf.com>
2020-07-12 10:37:38 +0900

    一時的なヘッダー作成

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/departures">departures</router-link>|
+      <router-link to="/check-in">check-in</router-link>|
+      <router-link to="/security-check">security-check</router-link>|
+      <router-link to="/immigration">immigration</router-link>|
+      <router-link to="/boarding-gate">boarding-gate</router-link>|
+      <router-link to="/on-board">on-board</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
