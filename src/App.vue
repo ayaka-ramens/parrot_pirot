98aa78caf4c505443cd6a9f2fbf344a552a48f90
everyleaf <a.moronaga@everyleaf.com>
2020-07-13 09:49:00 +0900

    ナビバーからSecurityCheck削除

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/departure">departure</router-link>|
       <router-link to="/check-in">check-in</router-link>|
-      <router-link to="/security-check">security-check</router-link>|
       <router-link to="/immigration">immigration</router-link>|
       <router-link to="/boarding-gate">boarding-gate</router-link>|
       <router-link to="/on-board">on-board</router-link>
