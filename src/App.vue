50fdd8be015847193e106036f58c37c29b424982
Ayaka Moronaga <a.moronaga@everyleaf.com>
2020-08-17 11:54:20 +0900

    navbar削除

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/departure">departure</router-link>|
-      <router-link to="/check-in">check-in</router-link>|
-      <router-link to="/immigration">immigration</router-link>|
-      <router-link to="/boarding-gate">boarding-gate</router-link>|
-      <router-link to="/on-board">on-board</router-link>
     </div>
     <router-view/>
   </div>
